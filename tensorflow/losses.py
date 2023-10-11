3282ec5f30df001063cf0570d353f1240a9ee712
Uroboros0313 <ouroboros_csl@163.com>
2023-10-12 05:13:47 +0800

    add tf lambdaLoss@K

--- tensorflow/losses.py
+++ tensorflow/losses.py
@@ -121,6 +121,68 @@ class ListNetLoss():
         return loss_
 
 
+class LambdaLoss():
+    def __init__(self, k=None) -> None:
+        self.k = k  # NDCG@K
+
+    def __call__(self, y_true, logits, uid_tensor, eps=1e-4):
+        return self.__loss_fn__(y_true, logits, uid_tensor, eps)
+
+    def __loss_fn__(self, y_true, logits, uid_tensor, eps):
+        uid_mask = tf.cast(tf.equal(uid_tensor, tf.transpose(uid_tensor, perm=[1, 0])), tf.float32)
+
+        # uid内部排序
+        gap_values = tf.reduce_sum(uid_mask, axis=-1, keepdims=True)
+        gap_y_true = y_true - gap_values
+        gap_y_pred = tf.sigmoid(logits) - gap_values
+
+        true_rank = tf.squeeze(tf.argsort(gap_y_true, axis=0, direction="DESCENDING"), axis=-1)
+        pred_rank = tf.squeeze(tf.argsort(gap_y_pred, axis=0, direction="DESCENDING"), axis=-1)
+
+        y_true_sorted = tf.gather(y_true, indices=true_rank)
+        true_sorted_by_pred = tf.gather(y_true, indices=pred_rank)
+        y_pred_sorted = tf.gather(logits, indices=pred_rank)
+
+        # Top1的折扣系数和uid掩码
+        cond_ = tf.cast(uid_mask, tf.bool) & tf.not_equal(tf.cumsum(uid_mask, axis=0), 1.0)
+        discount_score = tf.where(condition=cond_,
+                                  x=tf.math.log(tf.cumsum(uid_mask, axis=0) + 1.0 - tf.math.expm1(0.0)),
+                                  y=uid_mask)
+        discount_score = tf.reduce_sum(discount_score, axis=-1, keepdims=True) / \
+                         tf.reduce_sum(uid_mask, axis=-1, keepdims=True)
+
+        # 根据最佳排列计算IDCG
+        extended_y_true_sorted = y_true_sorted + tf.zeros_like(tf.transpose(y_true_sorted, perm=[1, 0]), tf.float32)
+        idcg = tf.reduce_sum(uid_mask * (extended_y_true_sorted / discount_score), axis=0)
+        idcg = tf.reshape(idcg, [-1, 1])
+
+        # Pair<i, j>交换:
+        # - 损失i, j原本的 (dcg_<i, rank_i> + dcg_<j, rank_j>) / idcg
+        # - 增加i, j交换的 (dcg_<i, rank_j> + dcg_<j, rank_i>) / idcg
+        row_plus_dcg = tf.transpose(true_sorted_by_pred, perm=[1, 0]) / discount_score
+        row_sub_dcg = true_sorted_by_pred / discount_score
+        plus_dcg = row_plus_dcg + tf.transpose(row_plus_dcg, perm=[1, 0])
+        sub_dcg = row_sub_dcg + tf.transpose(row_sub_dcg, perm=[1, 0])
+        ndcg_delta = tf.divide(plus_dcg - sub_dcg, idcg)
+        
+        # BPR Loss加权
+        label_mat = tf.cast(tf.greater(true_sorted_by_pred,
+                                       tf.transpose(true_sorted_by_pred, perm=[1, 0])), tf.float32)
+        logits_mat = tf.sigmoid(tf.subtract(y_pred_sorted, tf.transpose(logits, perm=[1, 0])))
+        loss_mat = label_mat * tf.math.log(logits_mat + eps) + \
+                   (1.0 - label_mat) * tf.math.log(1.0 - logits_mat + eps)
+
+        # NDCG@K的优化方法
+        if self.k != None:
+            topK_row_mask = tf.where(tf.cumsum(uid_mask, axis=0) <= self.k, 1.0, 0.0)
+            topK_mask = topK_row_mask * tf.transpose(topK_row_mask, perm=[1, 0])
+        else:
+            topK_mask = tf.ones_like(loss_mat, tf.float32)
+
+        loss_ = -tf.reduce_sum(tf.abs(ndcg_delta) * loss_mat * uid_mask * topK_mask)
+        return loss_
+
+
 if __name__ == '__main__':
     uid_tensor = tf.reshape(tf.constant([1, 1, 1, 4, 4, 4, 4]), [-1, 1])
     true_ = tf.reshape(tf.cast([0, 1, 1, 0, 0, 0, 1], dtype=tf.float32), [-1, 1])
@@ -133,3 +195,4 @@ if __name__ == '__main__':
     print(f"DistillRankingLoss:{DistillRankingLoss()(rels, preds, uid_tensor)}")
     print(f"WeightedDistillRankingLoss:{DistillRankingLoss()(rels, preds, uid_tensor, use_dvalue=True)}")
     print(f"BucketDistillRankingLoss:{BucketDistillRankingLoss()(rels, preds, uid_tensor)}")
+    print(f"LambdaLoss:{LambdaLoss(2)(rels, preds, uid_tensor)}")

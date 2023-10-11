ca88548cbe5c81a8203d6ceee147f683664e8a5a
Uroboros0313 <ouroboros_csl@163.com>
2023-10-12 00:32:08 +0800

    fix losses

--- tensorflow/losses.py
+++ tensorflow/losses.py
@@ -5,10 +5,10 @@ class DistillRankingLoss():
     def __init__(self) -> None:
         pass
 
-    def __call__(self, logits, y_true, uid_tensor, use_dvalue=False, eps=1e-4):
-        return self.__loss_fn__(logits, y_true, uid_tensor, use_dvalue, eps)
+    def __call__(self, y_true, logits, uid_tensor, use_dvalue=False, eps=1e-4):
+        return self.__loss_fn__(y_true, logits, uid_tensor, use_dvalue, eps)
 
-    def __loss_fn__(self, logits, y_true, uid_tensor, use_dvalue, eps):
+    def __loss_fn__(self, y_true, logits, uid_tensor, use_dvalue, eps):
         uid_mask = tf.cast(tf.equal(uid_tensor, tf.transpose(uid_tensor, perm=[1, 0])), tf.float32)
         dr_label_mat = tf.cast(tf.greater(y_true, tf.transpose(y_true, perm=[1, 0])), tf.float32)
         dr_logits_mat = tf.sigmoid(tf.subtract(logits, tf.transpose(logits, perm=[1, 0])))
@@ -36,19 +36,19 @@ class BucketDistillRankingLoss(DistillRankingLoss):
         norm_bucketized_values = tf.divide(tf.cast(bucket_idxs, tf.float32), self.num_bucket)
         return norm_bucketized_values
 
-    def __call__(self, logits, y_true, uid_tensor, use_dvalue=False, eps=1e-4):
+    def __call__(self, y_true, logits, uid_tensor, use_dvalue=False, eps=1e-4):
         bucketized_y_true = self.__bucketize__(y_true)
-        return self.__loss_fn__(logits, bucketized_y_true, uid_tensor, use_dvalue, eps)
+        return self.__loss_fn__(bucketized_y_true, logits, uid_tensor, use_dvalue, eps)
 
 
 class BprLoss():
     def __init__(self) -> None:
         pass
 
-    def __call__(self, logits, y_true, uid_tensor, eps=1e-4):
-        return self.__loss_fn__(logits, y_true, uid_tensor, eps)
+    def __call__(self, y_true, logits, uid_tensor, eps=1e-4):
+        return self.__loss_fn__(y_true, logits, uid_tensor, eps)
 
-    def __loss_fn__(self, logits, y_true, uid_tensor, eps):
+    def __loss_fn__(self, y_true, logits, uid_tensor, eps):
         uid_mask = tf.cast(tf.equal(uid_tensor, tf.transpose(uid_tensor, perm=[1, 0])), tf.float32)
         label_mask = tf.cast(tf.not_equal(y_true, tf.transpose(y_true, perm=[1, 0])), tf.float32)
 
@@ -77,10 +77,10 @@ class ListMLELoss():
     def __init__(self) -> None:
         pass
 
-    def __call__(self, logits, y_true, uid_tensor=None, eps=1e-4):
-        return self.__loss_fn__(logits, y_true, uid_tensor, eps)
+    def __call__(self, y_true, logits, uid_tensor=None, eps=1e-4):
+        return self.__loss_fn__(y_true, logits, uid_tensor, eps)
 
-    def __loss_fn__(self, logits, y_true, uid_tensor, eps):
+    def __loss_fn__(self, y_true, logits, uid_tensor, eps):
         indices = tf.argsort(y_true, axis=0, direction='DESCENDING')
         indices = tf.squeeze(indices, axis=-1)
 
@@ -102,10 +102,10 @@ class ListNetLoss():
     def __init__(self) -> None:
         pass
 
-    def __call__(self, logits, y_true, uid_tensor, eps=1e-4):
-        return self.__loss_fn__(logits, y_true, uid_tensor, eps)
+    def __call__(self, y_true, logits, uid_tensor, eps=1e-4):
+        return self.__loss_fn__(y_true, logits, uid_tensor, eps)
 
-    def __loss_fn__(self, logits, y_true, uid_tensor, eps):
+    def __loss_fn__(self, y_true, logits, uid_tensor, eps):
         logits = tf.math.exp(logits)
         uid_mask = tf.cast(tf.equal(uid_tensor, tf.transpose(uid_tensor, perm=[1, 0])), tf.float32)
         logits_mat = tf.transpose(logits, perm=[1, 0]) + tf.zeros_like(logits, dtype=tf.float32)
@@ -127,9 +127,9 @@ if __name__ == '__main__':
     rels = tf.reshape(tf.constant([0.01, 0.9, 0.87, 0.12, 0.10, 0.13, 0.68]), [-1, 1])
     preds = tf.reshape(tf.constant([0.5, 0.8, 0.2, 0.01, 0.23, 0.4, 0.1]), [-1, 1]) * 2 - 1
 
-    print(f"ListMLELoss:{ListMLELoss()(preds, rels, uid_tensor)}")
-    print(f"ListNetLoss:{ListNetLoss()(preds, true_, uid_tensor)}")
-    print(f"BprLoss:{BprLoss()(preds, true_, uid_tensor)}")
-    print(f"DistillRankingLoss:{DistillRankingLoss()(preds, rels, uid_tensor)}")
-    print(f"WeightedDistillRankingLoss:{DistillRankingLoss()(preds, rels, uid_tensor, use_dvalue=True)}")
-    print(f"BucketDistillRankingLoss:{BucketDistillRankingLoss()(preds, rels, uid_tensor)}")
+    print(f"ListMLELoss:{ListMLELoss()(rels, preds, uid_tensor)}")
+    print(f"ListNetLoss:{ListNetLoss()(true_, preds, uid_tensor)}")
+    print(f"BprLoss:{BprLoss()(true_, preds, uid_tensor)}")
+    print(f"DistillRankingLoss:{DistillRankingLoss()(rels, preds, uid_tensor)}")
+    print(f"WeightedDistillRankingLoss:{DistillRankingLoss()(rels, preds, uid_tensor, use_dvalue=True)}")
+    print(f"BucketDistillRankingLoss:{BucketDistillRankingLoss()(rels, preds, uid_tensor)}")

4771889e3180fdc0b5fc206ac35013aa0af1ee27
Uroboros0313 <ouroboros_csl@163.com>
2023-10-20 18:52:31 +0800

    add feature rank file

---
+++ cheatsheet/layers/feature_rank.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Layer, Dense
+from tensorflow.keras.regularizers import l2
+from tensorflow.keras.initializers import glorot_normal
+
+
+class SENet(Layer):
+    def __init__(self):
+        super().__init__()
+
+
+class CancelOut(Layer):
+    def __init__(self):
+        super().__init__()
+
+
+class DropRank(Layer):
+    def __init__(self):
+        super().__init__()
